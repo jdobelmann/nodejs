ac34909639ba28895c352f3919f12eb9ac7699be
Dobelmann <S536997@nwmissouri.edu>
2020-10-09 14:38:28 -0500

    Added html to index.js

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const port = process.env.PORT || 3002;
 // define server
 const server = http.createServer((request, res) => {
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World!  This is text - we can respond with HTML and JSON')
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<style>\
+    * {\
+        font-family: "Comic Sans MS", "Comic Sans", cursive;\
+        background-color: pink;\
+        color: purple;\
+    }\
+    </style>\
+    <h1>This is a Node.js demo</h1><br/><h2>This is the future</h2>')
 })
 
 server.listen(port, hostname, () => {
